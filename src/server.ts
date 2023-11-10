936d7ad0489f038031cc118cb5c297ba5a5b29d0
Borna Lindić <borna.lindic@fer.hr>
2023-11-10 21:56:35 +0100

    changes for deployment in creating server and package.json scripts

--- src/server.ts
+++ src/server.ts
@@ -11,13 +11,21 @@ app.set('view engine', 'pug');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4010;
 
 //definicija ruta
 app.use('/', homeRouter);
 
-
-const hostname = '127.0.0.1';
-const port = 4010;
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+if (externalUrl) {
+  const hostname = '0.0.0.0'; //ne 127.0.0.1
+  app.listen(port, hostname, () => {
+    console.log(`Server locally running at http://${hostname}:${port}/ and from
+    outside on ${externalUrl}`);}
+    );
+} else {
+  const hostname = '127.0.0.1';
+  app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
